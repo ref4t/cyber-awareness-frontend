ce0c93b3601c66b35f542ce4078329e01cd5f356
refat <md.abdullah.all.muktadir@gmail.com>
2025-07-09 04:21:48 +1000

    env added

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: process.env.REACT_APP_BASE_URL,
   withCredentials: true, // this ensures cookies (JWT) are included
 });
 
